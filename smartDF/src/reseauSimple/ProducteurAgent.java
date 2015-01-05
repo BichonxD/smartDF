6d2b4fea9315b4a58f1831d670232e94c91915c3
leodellouve <leo.dellouve@gmail.com>
2015-01-05 15:55:45 +0100

    changement droit & modif fourni

--- smartDF/src/reseauSimple/ProducteurAgent.java
+++ smartDF/src/reseauSimple/ProducteurAgent.java
@@ -1,6 +1,12 @@
 package reseauSimple;
 
-public class ProducteurAgent
-{
-	
-}
+import jade.core.Agent;
+
+public class ProducteurAgent extends Agent {
+
+	  protected void setup() {
+	  	System.out.println("Hello World! My name is "+getLocalName());
+	   	// Make this agent terminate
+	  	doDelete();
+	  } 
+	}
