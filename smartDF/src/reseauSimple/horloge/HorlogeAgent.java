e96008eb698320ac9dcaa39b374a0d77faeac365
PONCET Luc <lponcet@localhost.localdomain>
2015-01-26 15:16:25 +0100

    Modification de l'horloge.

--- smartDF/src/reseauSimple/horloge/HorlogeAgent.java
+++ smartDF/src/reseauSimple/horloge/HorlogeAgent.java
@@ -20,7 +20,7 @@ public class HorlogeAgent extends AbstractAgent
 		ServiceDescription SDAllAgents = new ServiceDescription();
 		rechercheAllAgents.addServices(SDAllAgents);
 		addBehaviour(new GlobalSearchBehaviour(this, rechercheAllAgents));
-		addBehaviour(new HorlogeBehaviourTalker());
+		addBehaviour(new HorlogeBehaviourTalker(this, "horloge-phase-negociation"));
 		addBehaviour(new HorlogeBehaviourListener());
 	}
 	
