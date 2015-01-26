52bcdc4c757b69a470a8a2be5fc087a3dc5379e2
PONCET Luc <lponcet@localhost.localdomain>
2015-01-26 16:22:00 +0100

    Changement de la gestion de l'horloge.

--- smartDF/src/reseauSimple/global/GlobalBehaviourHorlogeTalker.java
+++ smartDF/src/reseauSimple/global/GlobalBehaviourHorlogeTalker.java
@@ -20,28 +20,8 @@ public class GlobalBehaviourHorlogeTalker extends OneShotBehaviour
 	public void action()
 	{
 		ACLMessage reply = msgHorlogeToAnswer.createReply();
-		
-		switch(msgHorlogeToAnswer.getContent())
-		{
-			case "horloge-phase-negociation" :
-				reply.setContent("horloge-phase-negociation-termine");
-				myAgent.send(reply);
-				break;
-			
-			case "horloge-phase-facturaction" :
-				reply.setContent("horloge-phase-facturaction-termine");
-				myAgent.send(reply);
-				break;
-				
-			case "horloge-phase-departage" :
-				reply.setContent("horloge-phase-departage-termine");
-				myAgent.send(reply);
-				break;
-				
-			default :
-				System.err.println("Message reçu inconnu.");
-				break;
-		}
+		reply.setPerformative(msgHorlogeToAnswer.getPerformative());
+		myAgent.send(reply);
 	}
 	
 }
